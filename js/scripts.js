361e00faf6d3b3903432269b1f1f06c1445959ab
Seung Lee <shl7@uw.edu>
2022-06-01 13:31:59 -0700

    Add dog and cat image that's clickable and prints bark and meow
    Co-authored-by: Frank Timmons <franktimmonspdx@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+  $('.dog-closed').click(function () {
+    $(this).toggle();
+    $('.dog-open').toggle();
+    $('#dog-ul').prepend('<li>Bark</li>');
+    $('.cat-closed').toggle();
+    $('.cat-open').toggle();
+    $('#cat-ul').prepend('<li>Meow!!!!</li>');
+  });
+
+  $('.cat-closed').click(function () {
+    $(this).toggle();
+    $('.dog-open').toggle();
+    $('#dog-ul').prepend('<li>Bark!!!!</li>');
+    $('.dog-closed').toggle();
+    $('.cat-open').toggle();
+    $('#cat-ul').prepend('<li>Meow</li>');
+  });
+
+  $('.dog-open').click(function () {
+    $(this).toggle();
+    $('.dog-closed').toggle();
+    $('.cat-open').toggle();
+    $('.cat-closed').toggle();
+  });
+
+  $('.cat-open').click(function () {
+    $(this).toggle();
+    $('.dog-closed').toggle();
+    $('.dog-open').toggle();
+    $('.cat-closed').toggle();
+  });
+
+  $('ul#dog-ul')
+    .children('li')
+    .first()
+    .click(function () {
+      $(this).remove();
+    });
+  $('ul#cat-ul')
+    .children('li')
+    .first()
+    .click(function () {
+      $(this).remove();
+    });
+});
